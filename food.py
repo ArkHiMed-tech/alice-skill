e2be93fa23b1060aa901b5c901eacaf3200b237f
ArkHiMed-tech <74063313+ArkHiMed-tech@users.noreply.github.com>
2022-05-24 19:22:52 +0200

    Create food.py

---
+++ food.py
@@ -0,0 +1,95 @@
+food = {
+    'яблоко':{
+        'calories':52,#в килокалориях на 100гр
+        'countable':True,
+        'per_one':176#в граммах
+    },
+    'морковка':{
+        'calories':41,
+        'countable':True,
+        'per_one':150
+    },
+    'гречка':{
+        'calories':308,
+        'countable':False
+    },
+    'овсянка':{
+        'calories':379,
+        'countable':False
+    },
+    'рис':{
+        'calories':97,
+        'countable':False
+    },
+    'брокколи':{
+        'calories':34,
+        'countable':True,
+        'per_one':450#в граммах(тут один качан столько весит)
+    },
+    'гриб':{
+        'calories':27,#можно использовать за грибы в общем
+        'countable':True,
+        'per_one':30#вес одной штуки
+    },
+    'салат':{#овощной
+        'calories':12,
+        'countable':False
+    },
+    'огурец':{
+        'calories':15,#в килокалориях
+        'countable':True,
+        'per_one':132#в граммах
+    },
+    'клубника':{
+        'calories':41,#в килокалориях
+        'countable':True,
+        'per_one':15#в граммах
+    },
+    'помидор':{
+        'calories':18,
+        'countable':True,
+        'per_one':200
+    },
+    'сахар':{
+        'calories':47,
+        'countable':False
+    },
+    'пшенка':{
+        'calories':119,
+        'countable':False
+    },
+    'груша':{
+        'calories':57,
+        'countable':True,
+        'per_one':150
+    },
+    'чернослив':{
+        'calories': 231,
+        'countable': True,
+        'per_one': 35
+    },
+    'черника': {
+        'calories': 44,
+        'countable': True,
+        'per_one': 0.5
+    },
+    'творог':{
+        'calories':90,
+        'countable':False,
+    },
+    'абрикос':{
+        'calories':44,
+        'countable':True,
+        'per_one':53
+    },
+    'картофель':{
+        'calories':77,
+        'countable':True,
+        'per_one':100
+    },
+    'лук':{
+        'calories':40,
+        'countable':True,
+        'per_one':75
+    }
+}
